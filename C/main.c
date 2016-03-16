779e62565cf285c1b5398d47bf5619da16cbb172
Patrick José <patricjose@gmail.com>
2016-03-16 17:56:01 -0300

    Initial Commit

---
+++ C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C
+//
+//  Created by Patrick José on 16/03/16.
+//  Copyright © 2016 Patrick José. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
